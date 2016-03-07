6b0c7d2de042ef763388904e4c9be720664c9d87
TMac-Liu <tina@e.hunantv.com>
2016-03-07 18:01:36 +0800

    update uuid class

---
+++ system-ext/Uuid.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * Uuid类提供了 universally unique identifiers according to RFC 4122.
+ * Uuid提供 v3(), v4(),v5() 方法
+ * UUIDs as specified in RFC 4122
+ * 获取唯一Id,使用 uuid4()方法
+ * Supported versions of UUID is v4 (random), v3 (MD5 hash) and v5 (SHA-1 hash).
+ *
+ */
+class Uuid {
+
+    public static function v3($name, $namespace = null) {
+        if (is_null($namespace))
+            $namespace = Uuid::v4();
+
+        if (empty($name))
+            return FALSE;
+
+        if (!Uuid::is_valid($namespace))
+            return FALSE;
+
+        // Get hexadecimal components of namespace
+        $nhex = str_replace(array('-', '{', '}'), '', $namespace);
+
+        // Binary Value
+        $nstr = '';
+
+        // Convert Namespace UUID to bits
+        for ($i = 0; $i < strlen($nhex); $i += 2) {
+            $nstr .= chr(hexdec($nhex[$i] . $nhex[$i + 1]));
+        }
+
+        // Calculate hash value
+        $hash = md5($nstr . $name);
+        return sprintf('%08s-%04s-%04x-%04x-%12s',
+
+                // 32 bits for "time_low"
+                substr($hash, 0, 8),
+
+                // 16 bits for "time_mid"
+                substr($hash, 8, 4),
+
+                // 16 bits for "time_hi_and_version",
+                // four most significant bits holds version number 3
+                (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x3000,
+
+                // 16 bits, 8 bits for "clk_seq_hi_res",
+                // 8 bits for "clk_seq_low",
+                // two most significant bits holds zero and one for variant DCE1.1
+                (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+
+                // 48 bits for "node"
+                substr($hash, 20, 12)
+        );
+    }
+
+    public static function v4() {
+
+        //return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        return sprintf('%04x%04x%04x%04x%04x%04x%04x%04x',
+                // 32 bits for "time_low"
+                mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+                // 16 bits for "time_mid"
+                mt_rand(0, 0xffff),
+
+                // 16 bits for "time_hi_and_version",
+                // four most significant bits holds version number 4
+                mt_rand(0, 0x0fff) | 0x4000,
+
+                // 16 bits, 8 bits for "clk_seq_hi_res",
+                // 8 bits for "clk_seq_low",
+                // two most significant bits holds zero and one for variant DCE1.1
+                mt_rand(0, 0x3fff) | 0x8000,
+
+                // 48 bits for "node"
+                mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
+
+
+    public static function v5($name, $namespace = null) {
+        if (is_null($namespace))
+            $namespace = Uuid::v4();
+
+        if (empty($name))
+            return FALSE;
+
+        if (!Uuid::is_valid($namespace))
+            return FALSE;
+
+        // Get hexadecimal components of namespace
+        $nhex = str_replace(array('-', '{', '}'), '', $namespace);
+
+        // Binary Value
+        $nstr = '';
+
+        // Convert Namespace UUID to bits
+        for ($i = 0; $i < strlen($nhex); $i += 2) {
+            $nstr .= chr(hexdec($nhex[$i] . $nhex[$i + 1]));
+        }
+
+        // Calculate hash value
+        $hash = sha1($nstr . $name);
+
+        return sprintf('%08s-%04s-%04x-%04x-%12s',
+
+                // 32 bits for "time_low"
+                substr($hash, 0, 8),
+
+                // 16 bits for "time_mid"
+                substr($hash, 8, 4),
+
+                // 16 bits for "time_hi_and_version",
+                // four most significant bits holds version number 5
+                (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
+
+                // 16 bits, 8 bits for "clk_seq_hi_res",
+                // 8 bits for "clk_seq_low",
+                // two most significant bits holds zero and one for variant DCE1.1
+                (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+
+                // 48 bits for "node"
+                substr($hash, 20, 12)
+        );
+    }
+
+    public static function is_valid($uuid) {
+        return preg_match('/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/i', $uuid) === 1;
+    }
+}
